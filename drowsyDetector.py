38a670a8ec9fc8c75735d284d0c3012069e32201
Tommy D <tommy1344@vt.edu>
2025-06-05 23:36:35 -0400

    Initial Commit - added an imageCapture class

---
+++ drowsyDetector.py
@@ -0,0 +1,23 @@
+import torch
+from matplotlib import pyplot as plt
+import numpy as np
+import cv2
+import uuid
+import os
+import time
+
+model = torch.hub.load('ultralytics/yolov5','yolov5s')
+
+
+# accesses the webcam, reads the frames, closes if q is pressed
+cap = cv2.VideoCapture(0)
+while cap.isOpened():
+    ret, frame = cap.read()
+    results = model(frame)
+
+    cv2.imshow('Drowsiness Detector (q to exit)',np.squeeze(results.render()))
+
+    if cv2.waitKey(10) & 0xFF == ord('q'):
+        break
+cap.release()
+cv2.destroyAllWindows
---
+++ imageCapture.py
@@ -0,0 +1,33 @@
+import cv2
+import uuid
+import os
+import time
+
+IMAGES_PATH = os.path.join('data','images')
+labels = ['awake','drowsy']
+number_imgs = 20
+
+cap = cv2.VideoCapture(0)
+for label in labels:
+    print('Collecting images for {}'.format(label))
+    time.sleep(2)
+
+    img_count = 0
+    start_time = time.time()
+
+    # collects images and assigns them unique names according to class, identifier, and adds jpg
+    while img_count < number_imgs:
+        ret,frame = cap.read()
+        cv2.imshow('Image Collection', frame)
+        current_time = time.time()
+        if current_time - start_time >= 2:
+            img_count += 1
+            print('Collecting images for {}, image number {}'.format(label, img_count))
+            imgname = os.path.join(IMAGES_PATH, label + '.' + str(uuid.uuid1())+'.jpg')
+            cv2.imwrite(imgname,frame)
+            start_time = current_time
+
+        if cv2.waitKey(10) & 0xFF == ord('q'):
+                break
+cap.release()
+cv2.destroyAllWindows
